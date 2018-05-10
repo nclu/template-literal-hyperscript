b4d1238ebf2e345e8f66fbfc5eb562a7582e07f3
nathanieg lee-luders <nathaniel@luders.org>
2018-05-09 21:57:18 -0700

    passing basic tests

--- src/template-literal-hyperscript.js
+++ src/template-literal-hyperscript.js
@@ -1,35 +1,98 @@
-const templateLiteralHyperscript = {
-  greet() {
-    return 'hello';
-  },
-  splitLines(strs, ...params) {
-    let appendOrConcatenate = (arr, item) => {
-      if (arr.length) {
-        if (typeof arr[arr.length-1] == 'string' && typeof item == 'string') {
-          arr[arr.length-1] = arr[arr.length-1] + item;
-          return arr;
-        }
-        if (typeof arr[arr.length-1] == 'object' && typeof item == 'object') {
-          arr[arr.length-1] = Object.assign({}, arr[arr.length-1], item);
-          return arr;
-        }
-      }
-      arr.push(item);
-      return arr;      
-    }      
-    return strs.reduce((a, str, i) => {
-      let pcs = str.split('\n')
-      pcs.forEach((p,j) => {
-        let t = a[a.length - 1];
-        a[a.length - 1] = appendOrConcatenate(a[a.length - 1], p);
-        if (j < pcs.length - 1) a.push([]);
-      });
-      if (params && i < params.length) {
-        a[a.length - 1] = appendOrConcatenate(a[a.length - 1], params[i]);
-      }
-      return a;
-    }, [[]]);
-  }
+export const defaultConfig = {
+  matcher: /^(\s*)(\S*)\s*(\S.*)?$/,
+  textMatcher: /^(\s*)\$(.*)?$/
 };
 
-export default templateLiteralHyperscript;
+const appendOrConcatenate = (arr, item) => {
+  if (arr.length) {
+    if (typeof arr[arr.length-1] == 'string' && typeof item == 'string') {
+      arr[arr.length-1] = arr[arr.length-1] + item;
+      return arr;
+    }
+    if (typeof arr[arr.length-1] == 'object' && typeof item == 'object') {
+      arr[arr.length-1] = Object.assign({}, arr[arr.length-1], item);
+      return arr;
+    }
+  }
+  arr.push(item);
+  return arr;      
+}
+
+export const splitLines = function(strs, ...params) {      
+  return strs.reduce((a, str, i) => {
+    let pcs = str.split('\n')
+    pcs.forEach((p,j) => {
+      let t = a[a.length - 1];
+      a[a.length - 1] = appendOrConcatenate(a[a.length - 1], p);
+      if (j < pcs.length - 1) a.push([]);
+    });
+    if (params && i < params.length) {
+      a[a.length - 1] = appendOrConcatenate(a[a.length - 1], params[i]);
+    }
+    return a;
+  }, [[]]);
+};
+
+export const templateLiteralHyperscript = function(hyperscript) {
+  return function lh(strs, ...params) {
+    let lines = splitLines(strs, ...params);
+
+    let stack = lines.map(getAttrs).reduce((stack, elem, i) => {
+      //console.log(`${elem.whiteSpace} ${elem.element}-${elem.children.length}`, stack.map(l => `${l.whiteSpace} ${l.element}-${l.children.length}`))
+      if (i && elem.whiteSpace < stack[stack.length-1].whiteSpace) {
+        let chop = stack.findIndex(f => f.whiteSpace > elem.whiteSpace)
+        let t = assembleAll(stack.slice(chop,stack.length));
+        stack[chop-1].children.push(t);
+        stack.length = chop;
+        stack.push(elem);
+      } else {
+        //if (elem.whiteSpace == stack[stack.length-1].whiteSpace) {
+        //  stack[stack.length-1] = assembleElement(stack[stack.length-1]);
+        //}
+        stack.push(elem);
+      }
+      return stack;
+    }, []);
+    return assembleAll(stack);
+  };
+  
+  function assembleAll(stack) {
+    
+    return stack.reduceRight((a,elem, idx, arr) => {
+      if (idx == 0) {
+        elem.children.push(...a);
+        return assembleElement(elem);
+      }
+      if (idx == arr.length - 1 || elem.whiteSpace == arr[idx+1].whiteSpace) {
+        a.unshift(assembleElement(elem));
+        return a;
+      }
+      elem.children.push(...a);
+      return [assembleElement(elem)];
+    }, []);
+  }
+    
+  function assembleElement(elem) {
+    if (elem.hasOwnProperty('whiteSpace')) {
+      if (elem.hasOwnProperty('text')) {
+        return elem.text;
+      }
+      if (elem.hasOwnProperty('element')) {
+        return hyperscript(elem.element, ...elem.children);
+      }
+    }
+    return elem;
+  }
+  
+  function getAttrs(row) {
+    let matches = row[0].match(defaultConfig.matcher);
+    let attrs = {
+      whiteSpace: matches[1].length,
+      element: matches[2],
+      children: []
+    }
+    if (matches[3]) attrs.children.push(matches[3]);
+    attrs.children.push(...row.slice(1,row.length));
+    return attrs;
+  }
+};
--- test/unit/template-literal-hyperscript.js
+++ test/unit/template-literal-hyperscript.js
@@ -1,4 +1,5 @@
-import templateLiteralHyperscript from '../../src/template-literal-hyperscript';
+import {defaultConfig, splitLines, templateLiteralHyperscript} from '../../src/template-literal-hyperscript';
+import hyperscript from 'hyperscript';
 
 describe('templateLiteralHyperscript', () => {
   describe('Split lines function', () => {
@@ -6,18 +7,87 @@ describe('templateLiteralHyperscript', () => {
     });
 
     it('splits up a template into an array', () => {
-      let output = templateLiteralHyperscript.splitLines`div`;
+      let output = splitLines`div`;
       expect(output).to.deep.equal([['div']]);
-      output = templateLiteralHyperscript.splitLines`div
+      output = splitLines`div
         span`;
       expect(output).to.deep.equal([['div'],['        span']]);
-      output = templateLiteralHyperscript.splitLines`div${{class: 'test-class'}} cheese`;
+      output = splitLines`div${{class: 'test-class'}} cheese`;
       expect(output).to.deep.equal([['div',{class: 'test-class'}, ' cheese']]);
     });
     
     it('combines neighboring strings for its inputs', () => {
-      let output = templateLiteralHyperscript.splitLines`div.${'test-class'}`;
+      let output = splitLines`div.${'test-class'}`;
       expect(output).to.deep.equal([['div.test-class']]);
+    });
+  });
+  describe('the wrapper', () => {
+    it('gets the same output as hyperscript', () => {      
+      let t = templateLiteralHyperscript(hyperscript);
+      
+      expect(
+        t`foo`.outerHTML
+      ).to.eql(
+        hyperscript('foo').outerHTML
+      );
+      
+      expect(
+        t`div hello world`.outerHTML
+      ).to.eql(
+        hyperscript('div', 'hello world').outerHTML
+      );
+      
+      expect(
+        t`div
+            strong hello world`.outerHTML
+      ).to.eql(
+        hyperscript('div', 
+          hyperscript('strong', 'hello world')
+        ).outerHTML
+      );
+      
+      expect(
+        t`div${{class: 'nav-link'}}
+            a${{href:'/home'}}hello world`.outerHTML
+      ).to.eql(
+        hyperscript('div', {class: 'nav-link'}, 
+          hyperscript('a', {href:'/home'}, 'hello world')
+        ).outerHTML
+      );
+      
+      expect(
+        t`ul
+            li one
+            li two`.outerHTML
+      ).to.eql(
+        hyperscript('ul', 
+          hyperscript('li', 'one'),
+          hyperscript('li', 'two')
+        ).outerHTML
+      );
+      
+      console.log('test multiple branches');
+      expect(
+        t`div
+            h1 title
+              small sub-title
+                a ${{href:'/view/1'}}show
+            ul
+              li one
+              li two`.outerHTML
+      ).to.eql(
+        hyperscript('div',
+          hyperscript('h1', 'title',
+            hyperscript('small','sub-title',
+              hyperscript('a',{href:'/view/1'},'show')
+            )
+          ),
+          hyperscript('ul', 
+            hyperscript('li', 'one'),
+            hyperscript('li', 'two')
+          )
+        ).outerHTML
+      );
     });
   });
 });
